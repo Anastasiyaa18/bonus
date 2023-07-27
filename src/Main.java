605d634c1717dd3888cae1aee8a20c79bfde81fd
Анастасия Сергеевна <kuznecova_18@bk.ru>
2023-07-27 21:38:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13700;
+        int bonusru = 20;
+        int bonus = price / bonusru;
+
+        System.out.println("Бонусный баланс = " + bonus);
+    }
+}
